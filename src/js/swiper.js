e4b7694b533f567c958a42cc2d7fcea397379d1d
Mateusz Malec <m.mateusz2000@wp.pl>
2021-04-07 22:02:41 +0200

    feat: add dots for cards and style corrections

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -10,8 +10,11 @@ export const mySwiper = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
  
 });
 
